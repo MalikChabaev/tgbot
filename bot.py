acfa734338aac2cd3db602a9e7034ebc5675ebf0
Malik Chabaev <malikchabaev@gmail.com>
2025-05-24 20:54:29 +0300

    Добавил новую команду в бота

--- bot.py
+++ bot.py
@@ -7,10 +7,11 @@ bot = telebot.TeleBot('8034006021:AAFvaWnAUg70xucbRtxtEUxQd2GqbjeJcE4')
 def main(message):
     #  message может иметь любое имя. обязательно под хендлером
     # стоит его функция
-    bot.send_message(message.chat.id, f'{message.from_user.first_name}, го секс?')
+    bot.send_message(message.chat.id, f'{message.from_user.first_name}, привет!')
     # говорит отправить соо (из чата, который написал старт)
 
 bot.infinity_polling()
 # делает, чтобы он работал нонстоп
+#hi
     
     
