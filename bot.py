e71ca14058dfdfe4dd9dece7cf62f40c98b93839
Malik Chabaev <malikchabaev@gmail.com>
2025-05-26 16:19:37 +0300

    ервый коммит: добавлен бот

--- bot.py
+++ bot.py
@@ -9,38 +9,39 @@ def start(message):
     conn = sqlite3.connect('nandomo.sql')
     cur = conn.cursor()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primery key, name varchar(50), pass varchar(50))')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            pass TEXT
+        )
+    ''')
     conn.commit()
     cur.close()
     conn.close()
 
-    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегестрируем')
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем. Введите имя:')
     bot.register_next_step_handler(message, user_name)
 
 def user_name(message):
     global name
     name = message.text.strip()
-    bot.send_message(message.chat.id, 'Введите пароль')
-    bot.register_next_step_handler(message, user_name)
-    
+    bot.send_message(message.chat.id, 'Введите пароль:')
+    bot.register_next_step_handler(message, user_pass)
+
 def user_pass(message):
     password = message.text.strip()
     conn = sqlite3.connect('nandomo.sql')
     cur = conn.cursor()
 
-    cur.execute(
-        f'INSERT INTO users (name, pass) VALUES ({name}, {password})'
-        )
+    cur.execute('INSERT INTO users (name, pass) VALUES (?, ?)', (name, password))
     conn.commit()
     cur.close()
     conn.close()
 
     markup = telebot.types.InlineKeyboardMarkup()
     markup.add(telebot.types.InlineKeyboardButton('Список пользователей', callback_data='users'))
-    bot.send_message(message.chat.id, 'Пользователь зарегестрирован', reply_markup=markup)
-    # bot.register_next_step_handler(message, user_name)
+    bot.send_message(message.chat.id, 'Пользователь зарегистрирован!', reply_markup=markup)
 
 bot.remove_webhook()
 bot.polling(none_stop=True)
-    
-    
